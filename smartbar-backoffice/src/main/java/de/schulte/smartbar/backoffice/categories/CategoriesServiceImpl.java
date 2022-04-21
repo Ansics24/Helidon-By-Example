e2bb0f41a79dd1320aa04e4a601c193486bf9c59
Ansgar Schulte <info@ansgarschulte-itconsulting.de>
2022-04-21 21:01:25 +0200

    Configuration of simple category name

--- smartbar-backoffice/src/main/java/de/schulte/smartbar/backoffice/categories/CategoriesServiceImpl.java
+++ smartbar-backoffice/src/main/java/de/schulte/smartbar/backoffice/categories/CategoriesServiceImpl.java
@@ -1,13 +1,25 @@
 package de.schulte.smartbar.backoffice.categories;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @ApplicationScoped
 public class CategoriesServiceImpl implements CategoriesService {
+
+    private AtomicReference<String> categoryName = new AtomicReference<>();
+
+    @Inject
+    public CategoriesServiceImpl(@ConfigProperty(name = "app.categoryName") String categoryName) {
+        this.categoryName.set(categoryName);
+    }
+
     @Override
     public List<Category> listAll() {
-        return List.of(new Category("Drinks"), new Category("Food"));
+        return List.of(new Category(this.categoryName.get()));
     }
 }
